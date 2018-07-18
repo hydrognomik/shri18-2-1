22975ac3bc3124a44bba37c8c9d13d47bcece0cf
Aleksey Petrov <hydrognomik@gmail.com>
2018-07-18 21:04:54 +0300

    Исправил функцию инициализации карты.

--- src/map.js
+++ src/map.js
@@ -1,25 +1,22 @@
 import { loadList, loadDetails } from './api';
-import { getDetailsContentLayout } from './details';
-import { createFilterControl } from './filter';
+import getDetailsContentLayout from './details';
+import createFilterControl from './filter';
 
-export function initMap(ymaps, containerId) {
+export default function initMap(ymaps, containerId) {
   const myMap = new ymaps.Map(containerId, {
     center: [55.76, 37.64],
     controls: [],
-    zoom: 10
+    zoom: 11
   });
 
   const objectManager = new ymaps.ObjectManager({
     clusterize: true,
     gridSize: 64,
     clusterIconLayout: 'default#pieChart',
-    clusterDisableClickZoom: false,
-    geoObjectOpenBalloonOnClick: false,
-    geoObjectHideIconOnBalloonOpen: false,
     geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
   });
 
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+  myMap.geoObjects.add(objectManager);
 
   loadList().then(data => {
     objectManager.add(data);
@@ -44,7 +41,7 @@ export function initMap(ymaps, containerId) {
   const listBoxControl = createFilterControl(ymaps);
   myMap.controls.add(listBoxControl);
 
-  var filterMonitor = new ymaps.Monitor(listBoxControl.state);
+  const filterMonitor = new ymaps.Monitor(listBoxControl.state);
   filterMonitor.add('filters', filters => {
     objectManager.setFilter(
       obj => filters[obj.isActive ? 'active' : 'defective']
